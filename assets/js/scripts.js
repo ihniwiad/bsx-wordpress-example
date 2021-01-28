0aa61720cdaa0aefb4f680fe34c7a0d719827ba1
ihniwiad <mail@matthiasbroecker.de>
2021-01-28 13:53:36 +0100

    UPDATE: from basic package: add scroll window events

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3299,7 +3299,8 @@ link into hash tab:
     var Scrolling = {
         target: Utils.$body,
         position: 0,
-        direction: ''
+        direction: '',
+        place: ''
     };
 
     Scrolling.getPosition = function() {
@@ -3322,6 +3323,14 @@ link into hash tab:
 
     Scrolling.init = function() {
 
+        // TEST
+        // $( window ).on('scrollUp', function() { console.log( 'scrollUp' ) })
+        // $( window ).on('scrollDown', function() { console.log( 'scrollDown' ) })
+        // $( window ).on('scrollTop', function() { console.log( 'scrollTop' ) })
+        // $( window ).on('scrollNearTop', function() { console.log( 'scrollNearTop' ) })
+        // $( window ).on('scrollAwayTop', function() { console.log( 'scrollAwayTop' ) })
+        // $( window ).on('scrollBottom', function() { console.log( 'scrollBottom' ) })
+
         var defaults = {
             scrollDownClassName: 'scroll-down',
             scrollUpClassName: 'scroll-up',
@@ -3329,7 +3338,15 @@ link into hash tab:
             scrollBottomClassName: 'scroll-bottom',
             scrollNearTopClassName: 'scroll-near-top',
             scrollAwayTopClassName: 'scroll-away-top',
-            nearTopThreshold: 100
+            nearTopThreshold: 100,
+            triggerEvents: true,
+            scrollDownEventName: 'scrollDown',
+            scrollUpEventName: 'scrollUp',
+            scrollTopEventName: 'scrollTop',
+            scrollBottomEventName: 'scrollBottom',
+            scrollNearTopEventName: 'scrollNearTop',
+            scrollAwayTopEventName: 'scrollAwayTop'
+
         };
 
         var $elem = $( Scrolling.target );
@@ -3353,6 +3370,11 @@ link into hash tab:
             if ( currentDirection && Scrolling.direction != currentDirection ) {
                 if ( currentDirection == 'down' ) {
                     // scrolling down
+
+                    if ( options.triggerEvents ) {
+                        Utils.$window.trigger( options.scrollDownEventName );
+                    }
+                    
                     if ( ! $elem.is( '.' + options.scrollDownClassName ) ) {
                         $elem.addClass( options.scrollDownClassName );
                     }
@@ -3362,6 +3384,11 @@ link into hash tab:
                 }
                 else {
                     // scrolling up
+                    
+                    if ( options.triggerEvents ) {
+                        Utils.$window.trigger( options.scrollUpEventName );
+                    }
+                    
                     if ( ! $elem.is( '.' + options.scrollUpClassName ) ) {
                         $elem.addClass( options.scrollUpClassName );
                     }
@@ -3373,6 +3400,11 @@ link into hash tab:
 
             // check & set top class names
             if ( currentPosition == 0 ) {
+                if ( options.triggerEvents && Scrolling.place != 'top' ) {
+                    Utils.$window.trigger( options.scrollTopEventName );
+                    Scrolling.place = 'top';
+                }
+                    
                 if ( ! $elem.is( '.' + options.scrollTopClassName ) ) {
                     $elem.addClass( options.scrollTopClassName );
                 }
@@ -3383,20 +3415,14 @@ link into hash tab:
                 }
             }
 
-            // check & set bottom class name
-            if ( currentPosition + Utils.$window.height() >= Scrolling.target.height() ) {
-                if ( ! $elem.is( '.' + options.scrollBottomClassName ) ) {
-                    $elem.addClass( options.scrollBottomClassName );
-                }
-            }
-            else {
-                if ( $elem.is( '.' + options.scrollBottomClassName ) ) {
-                    $elem.removeClass( options.scrollBottomClassName );
-                }
-            }
-
             // check & set near away / class names
             if ( currentPosition < options.nearTopThreshold ) {
+                if ( options.triggerEvents && Scrolling.position != 0 && Scrolling.place != 'near-top' ) {
+                    // do not trigger near-top event if top
+                    Utils.$window.trigger( options.scrollNearTopEventName );
+                    Scrolling.place = 'near-top';
+                }
+
                 if ( ! $elem.is( '.' + options.scrollNearTopClassName ) ) {
                     $elem.addClass( options.scrollNearTopClassName );
                 }
@@ -3405,11 +3431,35 @@ link into hash tab:
                 }
             }
             else {
+                if ( options.triggerEvents && Scrolling.place != 'away-top' ) {
+                    Utils.$window.trigger( options.scrollAwayTopEventName );
+                    Scrolling.place = 'away-top';
+                }
+
                 if ( ! $elem.is( '.' + options.scrollAwayTopClassName ) ) {
                     $elem.addClass( options.scrollAwayTopClassName );
                 }
                 if ( $elem.is( '.' + options.scrollNearTopClassName ) ) {
                     $elem.removeClass( options.scrollNearTopClassName );
+                }
+            }
+
+            // check & set bottom class name
+            // place AFTER checking away-top to mak bottom event last while scrolling down (after away-top)
+            // round body height since might be larger than sum of both rounded scroll position and window height
+            if ( currentPosition + Utils.$window.height() >= Math.round( Scrolling.target.height() ) ) {
+                if ( options.triggerEvents && Scrolling.place != 'bottom' ) {
+                    Utils.$window.trigger( options.scrollBottomEventName );
+                    Scrolling.place = 'bottom';
+                }
+                    
+                if ( ! $elem.is( '.' + options.scrollBottomClassName ) ) {
+                    $elem.addClass( options.scrollBottomClassName );
+                }
+            }
+            else {
+                if ( $elem.is( '.' + options.scrollBottomClassName ) ) {
+                    $elem.removeClass( options.scrollBottomClassName );
                 }
             }
 
